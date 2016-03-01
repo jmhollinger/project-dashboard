f86e18c1794dbfa127b37abd6725609282da9ee8
Jonathan Hollinger <jmholl5@gmail.com>
2016-03-01 12:17:35 -0500

    Update

--- app.js
+++ app.js
@@ -111,15 +111,15 @@ app.get('/api/v1/department/id/:dept_id', function (req, res) {
 //Department by Name
 app.get('/api/v1/department/name/:dept_name', function (req, res) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query("SELECT DISTINCT department_id, department from divisions WHERE department_id = '" + req.params.dept_name + "'", function(err, result) {
+    client.query("SELECT DISTINCT department_id, department from divisions WHERE department = '" + req.params.dept_name + "'", function(err, result) {
       done();
       if (err)
        { console.error(err); 
-        res.json({"query" : "SELECT DISTINCT department_id, department from divisions WHERE department_id = '" + req.params.dept_name + "'", 
+        res.json({"query" : "SELECT DISTINCT department_id, department from divisions WHERE department = '" + req.params.dept_name + "'", 
           "error": err}); }
       else
        {res.json({
-        "query" : "SELECT DISTINCT department_id, department from divisions WHERE department_id = '" + req.params.dept_name + "'",
+        "query" : "SELECT DISTINCT department_id, department from divisions WHERE department = '" + req.params.dept_name + "'",
         "response" : result.rows
        }); }
     });
@@ -155,7 +155,7 @@ app.get('/api/v1/division/id/:div_id', function (req, res) {
 //Division by Name
 app.get('/api/v1/division/name/:div_name', function (req, res) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query("SELECT division from divisions WHERE division = '" + req.params.div_name + "'", function(err, result) {
+    client.query("SELECT division_id, division from divisions WHERE division = '" + req.params.div_name + "'", function(err, result) {
       done();
       if (err)
        { console.error(err); 
