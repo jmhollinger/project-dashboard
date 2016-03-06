3b988d680fea126c4082fcb667c2d206b88a4df2
Jonathan Hollinger <jmholl5@gmail.com>
2016-03-06 08:56:32 -0500

    Update

--- app.js
+++ app.js
@@ -365,6 +365,61 @@ app.get('/api/v1/project/:project_id/phase/:phase_id', function(req, res) {
     });
 });
 
+app.get('/api/v1/project/:project_id/phase/:phase_id', function(req, res) {
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+        client.query({
+            text: 'SELECT * FROM all_project_phases WHERE project_id = $1 AND phase_id = $2 ORDER BY start_date DESC;',
+            values: [req.params.project_id, req.params.phase_id]
+        }, function(err, result) {
+            done();
+            if (err) {
+                console.error(err);
+                res.json({
+                    'Error': err
+                });
+            } else {
+                var phaseData = result.rows;
+                
+                client.query({
+                    text: 'SELECT project_id, phase_id, phase_name FROM all_project_phases WHERE project_id = $1 ORDER BY start_date ASC;',
+                    values: [req.params.project_id]
+                }, function(err, result) {
+                    done();
+                    if (err) {
+                        console.error(err);
+                        res.json({
+                            'Error': err
+                        });
+                    } else {
+                        var phases = result.rows;
+                        
+                        client.query({
+                    text: 'SELECT DISTINCT date_modified, phase_id, notes FROM phases_history WHERE notes IS NOT NULL AND project_id = $1 AND phase_id = $2 ORDER BY date_modified DESC;',
+                    values: [req.params.project_id, req.params.phase_id]
+                }, function(err, result) {
+                    done();
+                    if (err) {
+                        console.error(err);
+                        res.json({
+                            'Error': err
+                        });
+                    } else {
+                        var notesHistory = result.rows;
+                    }
+
+                        res.json({
+                            "phases": phases,
+                            "phaseData": phaseData,
+                            "noteHistory" : notesHistory
+                        });
+                    }
+                });
+            }
+        });
+    });
+});
+
+
 //Server
 var server = app.listen(process.env.PORT || 3000, function() {
     var host = server.address().address;
