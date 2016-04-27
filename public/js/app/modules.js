ec92ef2679f457afeb4818034cc4f17835e65740
Jonathan Hollinger <jmholl5@gmail.com>
2016-04-26 21:25:37 -0400

    REVERT

--- public/js/app/modules.js
+++ public/js/app/modules.js
@@ -723,11 +723,11 @@ pdDirectives.directive('budgetBar', function () {
 projectDashboard.filter('titlecase', function () {
   return function (input) {
     var bigwords = /\b(LFUCG|ac|aka|llc|hvac|[a-z]\/[a-z]|i|ii|iii|iv|v|vi|vii|viii|ix)\b/i;
-	var smallwords = /\b(an|and|as|at|but|by|en|for|if|in|nor|of|on|or|per|to|vs)\b/i;
+  var smallwords = /\b(an|and|as|at|but|by|en|for|if|in|nor|of|on|or|per|to|vs)\b/i;
     var words = input.toLowerCase().split(' ');
     for (var i = 0; i < words.length; i++) {
       if (words[i].match(bigwords) !== null) {words[i] = words[i].toUpperCase()}
-      else if (words[i].match(smallwords) !== null)	{words[i] = words[i].toLowerCase()}
+      else if (words[i].match(smallwords) !== null) {words[i] = words[i].toLowerCase()}
       else {words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1)}
     }
     return words.join(' ');
