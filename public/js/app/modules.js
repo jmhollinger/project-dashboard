dcddecb2d8542ba0cbbde58ebab46f2863e196fd
Jonathan Hollinger <jmholl5@gmail.com>
2016-05-09 19:44:43 -0400

    Fix Comma

--- public/js/app/modules.js
+++ public/js/app/modules.js
@@ -588,7 +588,7 @@ pdControllers.controller('phaseEdit', ['$http','$scope', '$location', '$log', '$
 	    "phaseType" : [{"phase_type_id" : result.data.results[0].phase_status, "phase_name" : result.data.results[0].phase_name}],
       "phaseDesc" : result.data.results[0].phase_description,
 	    "phaseManager" : result.data.results[0].phase_manager,
-      "division" : [{"division" : result.data.results[0].division, "division_id" : result.data.results[0].division_id}]
+      "division" : [{"division" : result.data.results[0].division, "division_id" : result.data.results[0].division_id}],
 	    "resoNumber" : result.data.results[0].resolution_number,
 	    "accounting" : result.data.results[0].accounting,
 	    "rfpNumber" : result.data.results[0].rfp_number,
