bfb0d804b71707d9ad6c5ac35a4334bb70b7a31e
Jonathan Hollinger <jmholl5@gmail.com>
2016-05-09 22:42:01 -0400

    Update

--- public/js/app/modules.js
+++ public/js/app/modules.js
@@ -585,11 +585,11 @@ pdControllers.controller('phaseEdit', ['$http','$scope', '$location', '$log', '$
 	    "modifiedBy" : $scope.user.fullName,
       "phaseStatus" : {
           "status_type_id": result.data.results[0].status_type,
-          "status_name": result.data.results[0].status_name,
+          "status_name": result.data.results[0].status_name
           },
 	    "phaseType" : {
           "phase_type_id": result.data.results[0].phase_type,
-          "phase_name": result.data.results[0].phase_name,
+          "phase_name": result.data.results[0].phase_name
           },
       "phaseDesc" : result.data.results[0].phase_description,
 	    "phaseManager" : result.data.results[0].phase_manager,
